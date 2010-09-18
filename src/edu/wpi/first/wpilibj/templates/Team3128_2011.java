65f2d3180965797d644dc6645c37305d99725f1e
unknown <Dastyruck@.(none)>
2010-09-18 11:49:05 -0700

    first commit

---
+++ src/edu/wpi/first/wpilibj/templates/Team3128_2011.java
@@ -0,0 +1,30 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package edu.wpi.first.wpilibj.templates;
+
+
+import edu.wpi.first.wpilibj.SimpleRobot;
+
+public class Team3128_2011 extends SimpleRobot
+{
+    /**
+     * This function is called once each time the robot enters autonomous mode.
+     */
+    public void autonomousInit()
+    {
+        
+    }
+
+    /**
+     * This function is called once each time the robot enters operator control.
+     */
+    public void teleopInit()
+    {
+
+    }
+}
